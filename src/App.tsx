5f4f632e491759eed90eeb0931844015fc09e2ba
MauricioGamarraV <mauriciogamarravargas@gmail.com>
2026-02-24 23:37:54 -0400

    VC-10-set-common-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,47 @@
 import styles from './App.module.css';
 import { Button, Title, Content, Card, Input } from './components/common/index';
-import { House } from 'lucide-react';
+import { House, User, Lock } from 'lucide-react';
 
 function App() {
   return (
     <div className={styles.container}>
       <Title className={styles.title}>Villa Carito</Title>
 
-      <Title className={styles.customTitle}>Content</Title>
+      <Title className={styles.customTitle}>Inicio de sesion</Title>
 
       <Content>Content Text</Content>
-
-      <Button 
-        onClick={() => console.log('Custom Button')}
-        className={styles.button}
-      >
-        <House size={20} strokeWidth={"var(--ic-stroke-default)"} />
-        Custom Button
-      </Button>
 
       <Card>
         Card Info...
       </Card>
 
-      <Input></Input>
+      <Input
+        label='Username'
+        type='text'
+        classNameLabel={styles.inputTitle} 
+        classNameInput='' 
+        icon= {<User size={20} strokeWidth={"var(--ic-stroke-default)"} />}
+        placeholder='Ej. casa-7'
+        onChange={() =>{}}
+      />
+      
+      <Input
+        label='Password'
+        type='password'
+        classNameLabel={styles.inputTitle} 
+        classNameInput='' 
+        icon= {<Lock size={20} strokeWidth={"var(--ic-stroke-default)"} />}
+        placeholder='*****'
+        onChange={() =>{}}
+      />
+
+       <Button 
+        onClick={() => console.log('Custom Button')}
+        className={styles.button}
+      >
+        <House size={20} strokeWidth={"var(--ic-stroke-default)"} />
+          Login
+      </Button>
     </div>
   )
 }
--- src/components/common/custom input/Input.tsx
+++ src/components/common/custom input/Input.tsx
@@ -1,14 +1,36 @@
 import styles from './Input.module.css';
+import { Title } from '../index';
+import type { ReactNode } from 'react';
 
-const Input = () => {
+type Props = {
+  classNameLabel?: string;
+  classNameInput?: string;
+  onChange: () => void;
+  icon?: ReactNode;
+  label?: string;
+  placeholder?: string;
+  type: string;
+}
+
+const Input = ({ classNameLabel, classNameInput, onChange, icon, label, placeholder, type }: Props) => {
+  const labelClasses = `${styles.label} ${classNameLabel || ''}`;
+  const inputClasses = `${styles.input} ${classNameInput || ''}`;
+
   return (
-    <input 
-      className='' 
-      type="text" 
-      placeholder="text"
-      value={'Custom Input'}
-    />
+    <div className={styles.style}>
+      {label && <Title className={labelClasses}>{label}</Title>}
+      
+      <div className={styles.inputWrapper}>
+        {icon && <div className={styles.iconContainer}>{icon}</div>}
+        <input 
+          className={inputClasses} 
+          type={type} 
+          placeholder={placeholder || "Escribe aqui..."}
+          onChange={onChange}
+        />
+      </div>
+    </div>
   );
 }
 
-export { Input }
+export { Input };
