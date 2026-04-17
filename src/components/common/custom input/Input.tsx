c60f981598233e00ec3451c2744a4d46f4792af9
Mauricio Gamarra <140534752+mgamarra-assuresoft@users.noreply.github.com>
2026-04-16 20:54:32 -0400

    VC-10: Fix input component

--- src/components/common/custom input/Input.tsx
+++ src/components/common/custom input/Input.tsx
@@ -1,6 +1,6 @@
-import styles from './Input.module.css';
-import { Title } from '../index';
-import type { ReactNode } from 'react';
+import styles from "./Input.module.css";
+import { Title } from "../index";
+import type { ReactNode } from "react";
 
 type Props = {
   classNameLabel?: string;
@@ -10,27 +10,34 @@ type Props = {
   label?: string;
   placeholder?: string;
   type: string;
-}
+};
 
-const Input = ({ classNameLabel, classNameInput, onChange, icon, label, placeholder, type }: Props) => {
-  const labelClasses = `${styles.label} ${classNameLabel || ''}`;
-  const inputClasses = `${styles.input} ${classNameInput || ''}`;
+const Input = ({
+  classNameLabel,
+  classNameInput,
+  onChange,
+  icon,
+  label,
+  placeholder,
+  type,
+}: Props) => {
+  const labelClasses = `${styles.label} ${classNameLabel || ""}`;
+  const inputClasses = `${styles.input} ${classNameInput || ""}`;
 
   return (
     <div className={styles.style}>
       {label && <Title className={labelClasses}>{label}</Title>}
-      
       <div className={styles.inputWrapper}>
         {icon && <div className={styles.iconContainer}>{icon}</div>}
-        <input 
-          className={inputClasses} 
-          type={type} 
+        <input
+          className={inputClasses}
+          type={type}
           placeholder={placeholder || "Escribe aqui..."}
           onChange={onChange}
         />
       </div>
     </div>
   );
-}
+};
 
 export { Input };
