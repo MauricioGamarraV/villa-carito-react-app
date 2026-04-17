dea3cda643dd6ddff2332e5a55594e1a6334a8bc
Mauricio Gamarra Vargas <mauriciogamarravargas@gmail.com>
2026-04-16 22:23:56 -0400

    VC-10: add user login view (#14)
    Co-authored-by: Mauricio Gamarra <140534752+mgamarra-assuresoft@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,12 @@
-import styles from './App.module.css';
-import { Button, Title, Content, Card, Input } from './components/common/index';
-import { House, User, Lock } from 'lucide-react';
+import Login from "./components/login/Login";
+import styles from "./App.module.css";
 
 function App() {
   return (
     <div className={styles.container}>
-      <Title className={styles.title}>Villa Carito</Title>
-
-      <Title className={styles.customTitle}>Inicio de sesion</Title>
-
-      <Content>Content Text</Content>
-
-      <Card>
-        Card Info...
-      </Card>
-
-      <Input
-        label='Username'
-        type='text'
-        classNameLabel={styles.inputTitle} 
-        classNameInput='' 
-        icon= {<User size={20} strokeWidth={"var(--ic-stroke-default)"} />}
-        placeholder='Ej. casa-7'
-        onChange={() =>{}}
-      />
-      
-      <Input
-        label='Password'
-        type='password'
-        classNameLabel={styles.inputTitle} 
-        classNameInput='' 
-        icon= {<Lock size={20} strokeWidth={"var(--ic-stroke-default)"} />}
-        placeholder='*****'
-        onChange={() =>{}}
-      />
-
-       <Button 
-        onClick={() => console.log('Custom Button')}
-        className={styles.button}
-      >
-        <House size={20} strokeWidth={"var(--ic-stroke-default)"} />
-          Inicio de sesion
-      </Button>
+      <Login />
     </div>
-  )
+  );
 }
 
 export default App;
